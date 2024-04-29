3a1398ded2427160cf62f6d6d2c67f63ef85c902
Jirsaao <145552020+Jirsaao@users.noreply.github.com>
2024-04-29 19:53:00 +0200

    Añado campo vid

--- src/manager/Manager.java
+++ src/manager/Manager.java
@@ -23,7 +23,6 @@ import model.Vid;
 import utils.TipoVid;
 
 
-
 public class Manager {
 	private static Manager manager;
 	private ArrayList<Entrada> entradas;
@@ -33,7 +32,6 @@ public class Manager {
 	private Campo c;
 	private MongoDatabase database;
 	private MongoCollection<org.bson.Document> collection;
-
 
 	private Manager () {
 		this.entradas = new ArrayList<>();
@@ -47,7 +45,6 @@ public class Manager {
 	}
 	
 	private void createSession() {
-
 			Configuration configuration = new Configuration().configure("hibernate.cfg.xml"); 
 			org.hibernate.SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 			session = sessionFactory.openSession();
@@ -55,14 +52,13 @@ public class Manager {
 			MongoClientURI mongoClientURI = new MongoClientURI(uri);
 			MongoClient mongoClient = new MongoClient(mongoClientURI);
 			database = mongoClient.getDatabase("winery");
-		System.out.println("entro::::::::::::::::::::::::::::::::::::::::::::::::::");
 	}
 
 	public void init() {	
 		createSession();
 		getEntrada();
 		manageActions();
-		//sshowAllCampos();
+		showAllCampos();
 		session.close();
 	}
 
@@ -105,83 +101,37 @@ public class Manager {
 	// }
 
     private void addVid(String[] split) {
-	// 	Vid v = new Vid(TipoVid.valueOf(split[1].toUpperCase()), Integer.parseInt(split[2]));
-	// 	tx = session.beginTransaction();
-	// 	session.save(v);
+
+		System.out.println("vid añadido: " );
+		collection = database.getCollection("campo");
+		Document lastCampo = collection.find().sort(new Document("_id", -1)).first();
 		
-	// 	c.addVid(v);
-	// 	session.save(c);
-		
-	// 	tx.commit();
-		
-		System.out.println("VID****************************************");
-
-		Vid v = new Vid();
-
-		Document vidDoc = new Document()
-				.append("tipo_vid", split[1])  
-				.append("cantidad", Integer.parseInt(split[2]));
-	
-		// database.getCollection("vid").insertOne(vidDoc);
-	
-		// System.out.println("vid añadido: " + vidDoc);
-		// collection = database.getCollection("campo");
-		// Document lastCampo = collection.find().sort(new Document("_id", -1)).first();
-		
-		// collection = database.getCollection("vid");
-		// Document document  = new Document().append("tipo_vid", v.getVid().toString()).append("cantidad",v.getCantidad()).append("campo", lastCampo);
-		// collection.insertOne(document);
-
-		// Document document2 = new Document().append("tipo_vid", v.getVid().toString()).append("cantidad",v.getCantidad());
-
-		// collection = database.getCollection("campo");
-
-		// Document update = new Document("$push", new Document("Vids", document2));
-		// collection.updateOne(lastCampo, update);
+		collection = database.getCollection("vid");
+		Document document  = new Document().append("tipo_vid", split[1].toString())
+		.append("cantidad",Integer.parseInt(split[2])).append("campo", lastCampo);
+		collection.insertOne(document);
 
 	}
 	
 	private void addCampo(String[] split) {
-		// c = new Campo(b, split[1]);
-		// tx = session.beginTransaction();
-		// int id = (Integer) session.save(c);
-		// c = session.get(Campo.class, id);	
-		// tx.commit();
-		System.out.println("CAMPOOOO/////////////////");
-
 		Campo c = new Campo(b, split[1]);
 		collection = database.getCollection("bodega");//añado la ultima bodega
 		Document lastBodega = collection.find().sort(new Document("_id", -1)).first();
 
-
 		Document campoDoc = new Document("nombre", c.getNombre()).append("Bodega", lastBodega);
 		database.getCollection("campo").insertOne(campoDoc);
-
-		System.out.println("campo añadido: ");	
+	
 	}
 
 	 private void addBodega(String[] split) {
-	// 	b = new Bodega(split[1]);
-	// 	tx = session.beginTransaction();
-		
-	// 	int id = (Integer) session.save(b);
-	// 	b = session.get(Bodega.class, id);
-		
-	// 	tx.commit();
 		Bodega b = new Bodega(split[1]);
 	
 		Document bodegaDoc = new Document("nombre", b.getNombre());
 		database.getCollection("bodega").insertOne(bodegaDoc);
-	
-		System.out.println("Bodega añadida: ");
-	
+		
 	}
 	
 	private ArrayList<Entrada> getEntrada() {
-		//tx = session.beginTransaction();
-		//Query q = session.createQuery("select e from Entrada e");
-		//this.entradas.addAll(q.list());
-		//tx.commit();
 		collection = database.getCollection("entrada");
 		this.entradas = new ArrayList<>();
 		for (Document document : collection.find()){
@@ -189,11 +139,8 @@ public class Manager {
 			input.setInstruccion(document.getString("instruccion"));
 			this.entradas.add(input);
 		}
-
 		return entradas;
-
 	}
-	
 
 	private void showAllCampos() {
 		tx = session.beginTransaction();
@@ -205,5 +152,4 @@ public class Manager {
 		tx.commit();
 	}
 
-	
 }
